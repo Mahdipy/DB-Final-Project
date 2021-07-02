edfd96059273c54ade2e95172b4f6918351e8b3f
Mahdi <mahdipersia79@gmail.com>
2021-07-02 22:38:55 +0430

    Hash Password

--- main.py
+++ main.py
@@ -1,36 +1,60 @@
 import pymysql
-#database connection
-connection = pymysql.connect(host="localhost", user="root", passwd="", database="youtube")
-cursor = connection.cursor()
+import time
+import hashlib, uuid
 
-def signup():
-    with connection:
+
+# database connection
+
+
+class Main:
+    def __init__(self):
+        self.connection = pymysql.connect(host="localhost", user="root", passwd="", database="youtube")
+        self.cursor = self.connection.cursor()
+
+    def signup(self):
         username = ''
         pas = ''
         email = ''
-
-        while (username == ''):
-            username = input("Enter user name")
-            # cursor.execute("SELECT username FROM user")
-            # result = cursor.fetchone()
-            # while (result):
-            #     if result == username:
-            #         input("Enter another user name")
-            #     result = cursor.fetchone()
-
-        while pas == '':
-           pas = input("Enter password")
+        z = True
+        while (username == '') | z:
+            username = input("Enter Username: ")
+            self.cursor.execute("SELECT `username` FROM `user` where `username`=%s", (username,))
+            result = self.cursor.fetchone()
+            if result:
+                print('Username is already taken!')
+            else:
+                break
 
         while pas == '':
-          email = input("Enter email")
+            pas = input("Enter Password: ")
+
+        while email == '':
+            email = input("Enter Email: ")
+        hashed_password = hashlib.sha512((pas).encode('utf-8')).hexdigest()
+        self.cursor.execute("INSERT INTO `user` (`username`, `pass`, `email`, `date`) VALUES (%s, %s, %s, %s)",
+                            (username, hashed_password[:15], email, time.time()))
+
+        self.connection.commit()
+
+    def main(self):
+        x = input('Welcome!\n1)Login\n2)Sign Up\n')
+        if x == '1':
+            self.login()
+        elif x == '2':
+            self.signup()
+
+    def login(self):
+        name = input('Username: ')
+        password = input('Password: ')
+        hashed_password = hashlib.sha512((password).encode('utf-8')).hexdigest()
+        sql = "SELECT * FROM `user` WHERE `username`=%s and `pass`=%s"
+        self.cursor.execute(sql, (name, hashed_password[:15]))
+        result = self.cursor.fetchone()
+        if result:
+            print(f'Welcome {name}!')
+            while True:
+                z = input('enter your code: ')
 
 
-
-        cursor.execute("INSERT INTO user (username, pass, email) VALUES (%s, %s, %s)", (username, pas, email))
-
-        connection.commit()
-
-# def login():
-
-
-signup()
+x = Main()
+x.main()
